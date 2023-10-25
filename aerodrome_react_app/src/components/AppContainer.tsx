df856df0bb911cfac333a317592291066753764a
Mitchell <carroll.compute.inc@gmail.com>
2023-10-25 14:12:00 -0400

    Modified behaviour based on logged in state
    If user is logged in, show form, else user is not logged in, show table.

--- aerodrome_react_app/src/components/AppContainer.tsx
+++ aerodrome_react_app/src/components/AppContainer.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StatusTable from './StatusTable';
+import StatusForm from './StatusForm';
 import AerodromeImage from './AerodromeImage';
 import { initialData } from './StatusData';
-
-
 import './AppContainer.css';
 
 const AppContainer: React.FC = () => {
+  // Mock state for user authentication. In a real application,
+  // this state would be set based on the authentication system.
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
   return (
     <div className="app-container">
-      <StatusTable data={initialData} />
+      {isLoggedIn ? <StatusForm /> : <StatusTable data={initialData} />}
       <AerodromeImage />
+      {/* Mock button to toggle the isLoggedIn state for demonstration purposes */}
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Toggle Login State</button>
     </div>
   );
 }
