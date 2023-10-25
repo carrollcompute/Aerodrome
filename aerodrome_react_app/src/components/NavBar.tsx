288f72dfc87e5fc57643a3600944bb0437e0bfbb
Mitchell <carroll.compute.inc@gmail.com>
2023-10-25 12:18:59 -0400

    Switched to Vite + React + Typescript

---
+++ aerodrome_react_app/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const NavBar: React.FC = () => {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <img src="src\assets\atc.ico" alt="logo" height="40" width="40" />
+        
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
