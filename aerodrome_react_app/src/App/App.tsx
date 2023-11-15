0dbf7ec30230f314da4554ba91d76fce88f96404
CarrollCompute <carroll.compute.inc@gmail.com>
2023-11-15 15:35:02 -0500

    formatting

--- aerodrome_react_app/src/App/App.tsx
+++ aerodrome_react_app/src/App/App.tsx
@@ -4,6 +4,7 @@ import About from './About/About';
 import Status from './Status/Status';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { LoginProvider } from './LoginContext';
+import AerodromeImage from './Status/AerodromeImage';
 
 
 const App: React.FC = () => {
@@ -12,10 +13,17 @@ const App: React.FC = () => {
       <div>
         <LoginProvider>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Status />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
+          <div className="flex-container">
+            <Routes>
+              <Route path="/" element={<Status />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+            <AerodromeImage />
+            <Routes>
+              <Route path="/" element={<Status />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
         </LoginProvider>
       </div>
     </Router>
